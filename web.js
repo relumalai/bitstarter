f8fc02a1b5d61bfd7a27ad42d41c5d8094623bba
Ubuntu <ubuntu@ip-172-31-28-111.ap-southeast-1.compute.internal>
2013-07-12 08:46:10 +0000

    try 3

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var hw3 = require('fs');
-var teststr = hw3.readfilesync(index.html);
+//var hw3 = require('fs');
+//var teststr = hw3.readfilesync(index.html);
 
-var buffer = new Buffer(35);
-buffer.write ("Hello World from index.html", "utf-8");
+//var buffer = new Buffer(35);
+//buffer.write ("Hello World from index.html", "utf-8");
 
 
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
-    response.send(buffer.toString("utf-8"));
+  response.send('Hello World 2!');
+//    response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
