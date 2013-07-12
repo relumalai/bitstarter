9b383b14ff2c8158f2d886c5c6e36974b2ed1b63
Ubuntu <ubuntu@ip-172-31-28-111.ap-southeast-1.compute.internal>
2013-07-12 08:51:47 +0000

    try 5

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var hw3 = require('fs');
 var app = express.createServer(express.logger());
 //var hw3 = require('fs');
 var teststr = hw3.readfilesync(index.html);
-console.log(teststr);
+//console.log(teststr);
 //var buffer = new Buffer(35);
 //buffer.write ("Hello World from index.html", "utf-8");
 
